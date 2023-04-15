6143651f37783c974971e15e53a0f7711380658d
mikmet <mikko.metsaranta@orangit.fi>
2023-04-15 08:58:45 +0300

    Lint the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { type ReactElement, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../../../../../../../vite.svg';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App (): ReactElement {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev"
+          target="_blank" rel="noreferrer">
+          <img src={viteLogo}
+            className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => { setCount((count) => count + 1); }}>
           count is {count}
         </button>
         <p>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
