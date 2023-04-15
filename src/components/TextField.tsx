a149a1dfffb4d961173c5c5bced0d4ee1975d094
mikmet <mikko.metsaranta@orangit.fi>
2023-04-15 09:16:38 +0300

    TextField updates textState

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,8 +1,16 @@
 import React, { type ReactElement } from 'react';
+import { useRecoilState } from 'recoil';
+import textState from '../recoil/textState';
 
 const TextField = (): ReactElement => {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (value: string): void => {
+    setText(value);
+  };
+
   return (
-    <input type="text" />
+    <input type="text" value={text} onChange={(e) => { onChange(e.currentTarget.value); }} />
   );
 };
 
